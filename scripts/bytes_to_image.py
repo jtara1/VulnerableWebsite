1ab34edb318870a9d7b56d75e72ec016c0483597
James T <jtara@tuta.io>
2017-07-05 00:49:06 -0700

    fix bytes->image script (only tested w/ .png) & add modules req

--- scripts/bytes_to_image.py
+++ scripts/bytes_to_image.py
@@ -1,24 +1,18 @@
 from PIL import Image
 import io
-import os
 import sys
 import ast
 
 
-##file_name = "img-bytearray.txt"
+# use default file containing bytes if none given via cli
 file_name = "output.txt" if len(sys.argv) == 1 else sys.argv[1]
-##array = array.array('B')
-##array.fromfile(open(file_name, 'rb'), os.stat(file_name).st_size)
-with open(file_name, 'rb') as f:
-    bytes_read = ast.literal_eval(f.read())
-##    bytes_read = bytes(f.read())
 
+# read the string and evaluate it
+with open(file_name, 'r') as f:
+    file_bytes = ast.literal_eval(f.read())
 
-##image_bytes = io.BytesIO(array.tobytes())
-image_bytes = io.BytesIO(bytes_read)
-print('img bytes')
-##print(bytes_read)
-##print(image_bytes)
+image_bytes = io.BytesIO(file_bytes)
+# open image based on bytes from the file & save it
 image = Image.open(image_bytes)
 image.save("output-img.png")
 
