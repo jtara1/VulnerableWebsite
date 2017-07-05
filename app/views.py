147d940ff6a327688609e9523f625bfb1e2f0bcb
James T <jtara@tuta.io>
2017-07-04 18:45:51 -0700

    working as intended with major security flaw

--- app/views.py
+++ app/views.py
@@ -2,25 +2,33 @@ from flask import request
 from flask import render_template
 import pprint
 import json
+import os
+import sys
+import PIL
 from app import app
 
 
 @app.route('/')
-def my_form():
-    return render_template("my-form.html")
+def main():
+    """ Starting point when the web app is loaded """
+    return render_template("index.html")
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route('/submit', methods=['POST'])
+def input_handler():
+    """ Takes input from user """
     text = request.form['text']
     pretty_json = prettify_json(text)
-    return render_template("my-form.html", pretty_json=pretty_json)
+    return render_template("index.html", pretty_json=pretty_json)
 
 
 def prettify_json(text):
-    data = json.loads(str(text))
-    return pprint.pformat(data, indent=4, width=10)
-    # return eval(text)
+    """ Take some arbitrary string and convert it such that
+    string -> json_data_structure -> readable string representation
+    """
+    data = eval(text)
+    # data = json.loads(text.replace("'", '"'))
+    return pprint.pformat(data, indent=4, width=5)
 
 
 if __name__ == '__main__':
