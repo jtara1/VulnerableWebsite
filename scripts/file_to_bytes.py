f50f1d71d6c129e89fc6109574525a9a61a8ebf5
James T <jtara@tuta.io>
2017-07-06 23:33:00 -0700

    update file to bytes script

--- scripts/file_to_bytes.py
+++ scripts/file_to_bytes.py
@@ -7,13 +7,10 @@ if len(sys.argv) < 2:
 	raise Exception("no file name given via command line interface")
 
 file_name = sys.argv[1]
+output_file_name = sys.argv[2] if len(sys.argv) > 2 else "output.txt"
 
 # save bytes
 with open(file_name, 'rb') as infile:
-	with open('output.txt', 'w') as outfile:
-		img_bytes = infile.read()
-		outfile.write(str(bytes(img_bytes)))
+	with open(output_file_name, 'w') as outfile:
+		outfile.write(str(bytes(infile.read())))
 
-# save new image with the bytes
-# image = Image.open(io.BytesIO(img_bytes))
-# image.save('new_img.png')
