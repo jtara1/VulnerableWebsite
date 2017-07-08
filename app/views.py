d35780ca3dd081234b056f2b83bed50658991075
James T <jtara@tuta.io>
2017-07-07 20:07:11 -0700

    update to include another vulnerability (needs testing)

--- app/views.py
+++ app/views.py
@@ -1,10 +1,6 @@
-from flask import request
-from flask import render_template
+from flask import request, render_template
 import pprint
 import json
-import os
-import sys
-import PIL
 from app import app
 
 
@@ -24,12 +20,9 @@ def input_handler():
 
 def prettify_json(text):
     """ Take some arbitrary string and convert it such that
-    string -> json_data_structure -> readable string representation
+    string -> python data structure -> readable string representation
     """
-    data = eval(text)
-    # data = json.loads(text.replace("'", '"'))
+    # Vulnerability 1
+    data = exec(text)
+    # data = json.loads(text.replace("'", '"'))  # this alternative would fix Vulnerability 1
     return pprint.pformat(data, indent=4, width=5)
-
-
-if __name__ == '__main__':
-    app.run()
--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 from app import app
-app.run(debug=False)
+
+
+""" Vulnerability 1
+Run the app in debug mode so anyone on the network can access the running
+instance of the web app. Since debug mode is enable too, anyone can run arbitrary 
+Python code on the computer running this app
+"""
+app.run(debug=True, host='0.0.0.0')
+# safely run the app such that only the PC running it has access to webpage of the app
+# app.run(debug=False)
